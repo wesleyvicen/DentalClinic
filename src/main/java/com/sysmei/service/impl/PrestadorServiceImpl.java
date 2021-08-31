7791acdd29e83a8b7c08fc098b23bed8d6c7112b
Wesley Vicente <wesley1535@hotmail.com>
2021-08-30 23:19:30 -0300

    Teste

--- src/main/java/com/sysmei/service/impl/PrestadorServiceImpl.java
+++ src/main/java/com/sysmei/service/impl/PrestadorServiceImpl.java
@@ -36,7 +36,7 @@ public class PrestadorServiceImpl implements PrestadorService {
 
 	public Prestador getByTelefone(String telefone) {
 		Optional<Prestador> obj = prestadorRepository.findByTelefone(telefone);
-		if (obj.isEmpty()) {
+		if (obj.equals(obj.empty())) {
 			if (telefone.length() == 10) {
 				telefone = telefone.substring(0, 2) + 9 + telefone.substring(2, 10);
 			} else if (telefone.length() == 11) {
